6529224f2e8460019c7b3914fdaa70df3791c587
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-03-09 15:29:46 +0300

    переименовал главную активити перенес ее в нужный пакет (плезентационный слой)

--- app/src/main/java/com/example/movieslistapp/presentation/MainSingleActivity.kt
+++ app/src/main/java/com/example/movieslistapp/presentation/MainSingleActivity.kt
@@ -1,9 +1,10 @@
-package com.example.movieslistapp
+package com.example.movieslistapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movieslistapp.R
 
-class MainActivity : AppCompatActivity() {
+class MainSingleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

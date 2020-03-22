e18c6878f8d9a81d34b8517f204fd5ffe3dbcf48
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-03-22 21:20:08 +0300

    добавил тест AuthInterceptor

--- core/src/main/java/com/example/core/network/AuthInterceptor.kt
+++ core/src/main/java/com/example/core/network/AuthInterceptor.kt
@@ -7,7 +7,7 @@ import okhttp3.Response
 class AuthInterceptor : Interceptor {
 
     companion object {
-        private const val API_TOKEN_QUERY_FIELD_NAME = "api_key"
+        const val API_TOKEN_QUERY_FIELD_NAME = "api_key"
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {

614ffb7b4955919b64c35d85bf548a72a128a792
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-03-22 16:08:41 +0300

    перенес логику проверки сессии в appPresenter

--- app/src/main/java/com/example/movieslistapp/presentation/AppActivity.kt
+++ app/src/main/java/com/example/movieslistapp/presentation/AppActivity.kt
@@ -1,33 +1,39 @@
-package com.example.movieslistapp
+package com.example.movieslistapp.presentation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.example.core.di.CoreComponentHolder
 import com.example.feature_mainscreen.SessionMockFragment
 import com.example.feaure_authorization.presentation.view.AuthFragment
+import com.example.movieslistapp.R
+import com.example.movieslistapp.di.DaggerAppComponent
+import moxy.MvpAppCompatActivity
+import moxy.ktx.moxyPresenter
+import javax.inject.Inject
+import javax.inject.Provider
 
-class AppActivity : AppCompatActivity() {
+class AppActivity : MvpAppCompatActivity(), AppView {
+
+    @Inject
+    lateinit var presenterProvider: Provider<AppPresenter>
+    private val presenter by moxyPresenter { presenterProvider.get() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
+        DaggerAppComponent.factory()
+            .create(CoreComponentHolder.coreComponent)
+            .inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val userPrefs = CoreComponentHolder.coreComponent.provideUserPrefs()
-        if (userPrefs.sessionId != null) {
-            openMainScreen()
-        } else {
-            openAuthScreen()
-        }
     }
 
-    private fun openAuthScreen() {
+    override fun openAuthScreen() {
         supportFragmentManager.commit(true) {
             replace(R.id.fragment_container, AuthFragment.newInstance())
         }
     }
 
-    private fun openMainScreen() {
+    override fun openMainScreen() {
         supportFragmentManager.commit(true) {
             replace(R.id.fragment_container, SessionMockFragment.newInstance())
         }

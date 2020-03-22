f32137dc7dbba1bb34de8c50d3b75e43395d96ef
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-03-22 20:36:14 +0300

    добавил простой тест с использованием spec 2

--- core/src/test/java/com/example/core/util/CollectionExtTest.kt
+++ core/src/test/java/com/example/core/util/CollectionExtTest.kt
@@ -1,14 +1,50 @@
 package com.example.core.util
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.assertj.core.api.Assertions.catchThrowable
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
 
-class CollectionExtTest {
+object CollectionExtTest : Spek(
+    {
+        Feature("swap") {
 
-    @Test
-    fun `test swap extension`() {
-        val a = mutableListOf(1, 2, 3)
-        a.swap(1, 2)
-        assertThat(a).isEqualTo(mutableListOf(1, 3, 2))
+            Scenario("swap elements in non empty list") {
+                val sutList by memoized { mutableListOf<String>() }
+                val a = "a"
+                val b = "b"
+                val c = "c"
+                Given("list of three elements: a, b, c") {
+                    sutList.addAll(listOf(a, b, c))
+                }
+                When("swap a and b") {
+                    sutList.swap(0, 1)
+                }
+                Then("index of a should be 1") {
+                    assertThat(sutList.indexOf(a)).isEqualTo(1)
+                }
+                Then("index of b should be 0") {
+                    assertThat(sutList.indexOf(b)).isEqualTo(0)
+                }
+                Then("index of c should be 2") {
+                    assertThat(sutList.indexOf(c)).isEqualTo(2)
+                }
+                Then("size of list should be same (3)") {
+                    assertThat(sutList.indexOf(c)).isEqualTo(2)
+                }
+            }
+
+            Scenario("swap elements in empty list") {
+                val sutList = mutableListOf<String>()
+                lateinit var exception: Throwable
+                When("swap elements in empty list") {
+                    exception = catchThrowable { sutList.swap(0, 1) }
+                }
+                Then("throw exception") {
+                    assertThat(exception).isNotNull()
+                }
+            }
+        }
     }
-}
+)
+

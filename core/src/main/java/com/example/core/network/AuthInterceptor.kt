aaf98e52f4c45ae1007fe7d4efb38a1e6f1f6f19
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-03-22 16:41:45 +0300

    вынес api token в билд константу

--- core/src/main/java/com/example/core/network/AuthInterceptor.kt
+++ core/src/main/java/com/example/core/network/AuthInterceptor.kt
@@ -1,15 +1,21 @@
 package com.example.core.network
 
+import com.example.core.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor : Interceptor {
+
+    companion object {
+        private const val API_TOKEN_QUERY_FIELD_NAME = "api_key"
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
 
         val originalRequest = chain.request()
 
         val newUrl = originalRequest.url.newBuilder()
-            .addQueryParameter("api_key", "e7dc1eb39c088f9eeb3e4f5b775af371")
+            .addQueryParameter(API_TOKEN_QUERY_FIELD_NAME, BuildConfig.API_TOKEN)
             .build()
 
         val newRequest = originalRequest.newBuilder()

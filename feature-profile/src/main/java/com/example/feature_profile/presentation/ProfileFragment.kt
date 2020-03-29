2a1978e89b04f8fdc857ecdfc389850160146e02
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-03-29 21:54:36 +0300

    binding

---
+++ feature-profile/src/main/java/com/example/feature_profile/presentation/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package com.example.feature_profile.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.core.presentation.BaseFragment
+import com.example.feature_profile.databinding.FragmentProfileBinding
+
+class ProfileFragment : BaseFragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}

e1c275e196cdd618c25c0e2ca72f2cefa022a44e
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-03-31 22:29:56 +0300

    make splash fragment root

--- app/src/main/java/com/example/movieslistapp/presentation/AppActivity.kt
+++ app/src/main/java/com/example/movieslistapp/presentation/AppActivity.kt
@@ -1,22 +1,12 @@
 package com.example.movieslistapp.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.commit
 import com.example.core.di.CoreComponentHolder
-import com.example.feature_profile.presentation.ProfileFragment
-import com.example.feaure_authorization.presentation.view.AuthFragment
 import com.example.movieslistapp.R
 import com.example.movieslistapp.di.DaggerAppComponent
 import moxy.MvpAppCompatActivity
-import moxy.ktx.moxyPresenter
-import javax.inject.Inject
-import javax.inject.Provider
 
-class AppActivity : MvpAppCompatActivity(), AppView {
-
-    @Inject
-    lateinit var presenterProvider: Provider<AppPresenter>
-    private val presenter by moxyPresenter { presenterProvider.get() }
+class AppActivity : MvpAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -25,17 +15,5 @@ class AppActivity : MvpAppCompatActivity(), AppView {
             .inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun openAuthScreen() {
-        supportFragmentManager.commit(true) {
-            replace(R.id.fragment_container, AuthFragment.newInstance())
-        }
-    }
-
-    override fun openMainScreen() {
-        supportFragmentManager.commit(true) {
-            replace(R.id.fragment_container, ProfileFragment.newInstance())
-        }
     }
 }
--- app/src/main/java/com/example/movieslistapp/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/example/movieslistapp/presentation/splash/SplashFragment.kt
@@ -1,12 +1,15 @@
 package com.example.movieslistapp.presentation.splash
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.core.di.CoreComponentHolder
 import com.example.core.presentation.BaseFragment
 import com.example.core.util.observe
+import com.example.movieslistapp.R
 import com.example.movieslistapp.di.DaggerAppComponent
 import javax.inject.Inject
 
@@ -21,6 +24,14 @@ class SplashFragment : BaseFragment() {
             .create(CoreComponentHolder.coreComponent)
             .inject(this)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

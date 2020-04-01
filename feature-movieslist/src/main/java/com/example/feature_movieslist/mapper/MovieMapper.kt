a6f520510fcadce37f52c2e8f32442fde4ca977c
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-04-02 00:59:15 +0300

    add image loading

--- feature-movieslist/src/main/java/com/example/feature_movieslist/mapper/MovieMapper.kt
+++ feature-movieslist/src/main/java/com/example/feature_movieslist/mapper/MovieMapper.kt
@@ -1,5 +1,6 @@
 package com.example.feature_movieslist.mapper
 
+import com.example.core.consts.IMAGE_BASE_URL
 import com.example.feature_movieslist.data.dto.MovieDto
 import com.example.feature_movieslist.domain.MovieEntity
 import javax.inject.Inject
@@ -8,7 +9,7 @@ class MovieMapper @Inject constructor() {
     fun map(movieDto: MovieDto): MovieEntity {
         return MovieEntity(
             id = movieDto.id,
-            posterUrl = movieDto.poster_path,
+            posterUrl = IMAGE_BASE_URL + movieDto.poster_path,
             title = movieDto.title,
             subtitle = movieDto.original_title,
             genre = movieDto.genre_ids.toString(), // todo load genres
--- feature-movieslist/src/main/java/com/example/feature_movieslist/presentation/list/MovieViewHolder.kt
+++ feature-movieslist/src/main/java/com/example/feature_movieslist/presentation/list/MovieViewHolder.kt
@@ -1,12 +1,25 @@
 package com.example.feature_movieslist.presentation.list
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.feature_movieslist.R
 import com.example.feature_movieslist.databinding.ItemMovieLineBinding
 import com.example.feature_movieslist.domain.MovieEntity
 
 class MovieViewHolder(
     private val movieBinding: ItemMovieLineBinding
 ) : RecyclerView.ViewHolder(movieBinding.root) {
+
+    private val placeholder: Drawable
+
+    init {
+        placeholder = ColorDrawable(
+            ContextCompat.getColor(itemView.context, R.color.dark_blue)
+        )
+    }
 
     fun bind(viewModel: MovieEntity) {
         movieBinding.movieTitle.text = viewModel.title
@@ -15,5 +28,9 @@ class MovieViewHolder(
         movieBinding.movieRating.text = viewModel.rating.toString()
         movieBinding.movieRatingsCount.text = viewModel.ratingCount.toString()
         movieBinding.movieDuration.text = viewModel.duration
+        Glide.with(itemView)
+            .load(viewModel.posterUrl)
+            .placeholder(placeholder)
+            .into(movieBinding.moviePoster)
     }
 }

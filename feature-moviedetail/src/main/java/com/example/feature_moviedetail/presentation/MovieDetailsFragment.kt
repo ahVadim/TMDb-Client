e5fbe8ab12d6b119a36b6b6cc0530b1d0b843812
Vadim Akhtiamov (intern) <datamod93@gmail.com>
2020-04-07 23:23:33 +0300

    pass movie to view model

--- feature-moviedetail/src/main/java/com/example/feature_moviedetail/presentation/MovieDetailsFragment.kt
+++ feature-moviedetail/src/main/java/com/example/feature_moviedetail/presentation/MovieDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.feature_moviedetail.presentation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.example.core.di.CoreComponentHolder
 import com.example.core.presentation.BaseFragment
+import com.example.core.util.observe
 import com.example.feature_moviedetail.databinding.FragmentMoviedetailsBinding
+import com.example.feature_moviedetail.di.DaggerMoviesDetailsComponent
 import javax.inject.Inject
 
 class MovieDetailsFragment : BaseFragment() {
@@ -17,15 +21,38 @@ class MovieDetailsFragment : BaseFragment() {
     internal lateinit var viewModelFactory: ViewModelProvider.Factory
     private val movieDetailsViewModel: MovieDetailsViewModel by viewModels { viewModelFactory }
 
+    private var _binding: FragmentMoviedetailsBinding? = null
+    private val binding get() = _binding!!
+
     private val args: MovieDetailsFragmentArgs by navArgs()
+
+    override fun onAttach(context: Context) {
+        DaggerMoviesDetailsComponent.factory()
+            .create(CoreComponentHolder.coreComponent)
+            .inject(this)
+        super.onAttach(context)
+        movieDetailsViewModel.setMovie(args.movieEntity)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentMoviedetailsBinding.inflate(inflater, container, false)
-        //        binding.movieTitle.text = args.movieName
+        _binding = FragmentMoviedetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observe(movieDetailsViewModel.liveState, ::renderState)
+    }
+
+    private fun renderState(state: MovieDetailsViewState) {
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
--- feature-moviedetail/src/main/java/com/example/feature_moviedetail/presentation/MovieDetailsViewModel.kt
+++ feature-moviedetail/src/main/java/com/example/feature_moviedetail/presentation/MovieDetailsViewModel.kt
@@ -1,10 +1,17 @@
 package com.example.feature_moviedetail.presentation
 
 import androidx.lifecycle.MutableLiveData
+import com.example.core.domain.MovieEntity
 import com.example.core.presentation.BaseViewModel
+import com.example.core.util.delegate
 import javax.inject.Inject
 
 class MovieDetailsViewModel @Inject constructor() : BaseViewModel() {
 
-    var liveState = MutableLiveData<MovieDetailsViewState>()
+    val liveState = MutableLiveData<MovieDetailsViewState>()
+    private var state by liveState.delegate()
+
+    fun setMovie(movie: MovieEntity) {
+        state = MovieDetailsViewState(movie)
+    }
 }

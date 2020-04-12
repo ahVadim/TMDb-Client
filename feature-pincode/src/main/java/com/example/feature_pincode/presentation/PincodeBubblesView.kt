a230a3f3a6f0dd7d7600ec882cc9110865e9639a
v.akhtyamov <v.akhtyamov@city-mobil.ru>
2020-04-12 13:45:52 +0300

    create custom view for pincode fragment

--- feature-pincode/src/main/java/com/example/feature_pincode/presentation/PincodeBubblesView.kt
+++ feature-pincode/src/main/java/com/example/feature_pincode/presentation/PincodeBubblesView.kt
@@ -2,7 +2,11 @@ package com.example.feature_pincode.presentation
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import com.example.feature_pincode.R
 
 class PincodeBubblesView @JvmOverloads constructor(
     context: Context,
@@ -10,7 +14,36 @@ class PincodeBubblesView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
+    companion object {
+        private const val DEFAULT_BUBBLES_COUNT = 4
+    }
+
+    private val bubblesCount: Int
+
     init {
         orientation = HORIZONTAL
+        if (attrs != null) {
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.PincodeBubblesView)
+            bubblesCount = typedArray.getInteger(
+                R.styleable.PincodeBubblesView_bubbles_count,
+                DEFAULT_BUBBLES_COUNT
+            )
+            typedArray.recycle()
+        } else {
+            bubblesCount = DEFAULT_BUBBLES_COUNT
+        }
+
+        dividerDrawable = ContextCompat.getDrawable(context, R.drawable.pincode_bubble_divider)
+        showDividers = SHOW_DIVIDER_MIDDLE
+
+        val bubbleSize = resources.getDimensionPixelSize(R.dimen.pincode_bubble_size)
+
+        (0 until bubblesCount).forEach { _ ->
+            val imageView = ImageView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(bubbleSize, bubbleSize)
+                setImageResource(R.drawable.pincode_bubble_bg)
+            }
+            this.addView(imageView)
+        }
     }
 }
--- feature-pincode/src/main/java/com/example/feature_pincode/presentation/PincodeFragment.kt
+++ feature-pincode/src/main/java/com/example/feature_pincode/presentation/PincodeFragment.kt
@@ -1,6 +1,18 @@
 package com.example.feature_pincode.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.core.presentation.BaseFragment
+import com.example.feature_pincode.databinding.FragmentPincodeBinding
 
 class PincodeFragment: BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentPincodeBinding.inflate(inflater, container, false).root
+    }
 }

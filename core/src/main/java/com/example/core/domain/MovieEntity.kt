2b6d17ffa2113ffa9c5703205c1ce7e922a19d55
vadim <datamod93@gmail.com>
2024-04-05 18:51:47 +0500

    Update dependencies

--- core/src/main/java/com/example/core/domain/MovieEntity.kt
+++ core/src/main/java/com/example/core/domain/MovieEntity.kt
@@ -2,7 +2,7 @@ package com.example.core.domain
 
 import android.os.Parcelable
 import androidx.annotation.Keep
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Keep
 @Parcelize
